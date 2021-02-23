d2a95260dfae8f354d849b3a00daecfa9fbc5310
Diego Orefici <orefici.diego@gmail.com>
2021-02-23 11:13:10 -0300

    Middlewares added. DB required to synch.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+require("./db");
+
+app.use("/api", require("./router/api/posts"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`);
 });
