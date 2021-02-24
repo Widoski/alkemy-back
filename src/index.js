babb99ef7f4ccf3777474b94dd8cdf6e33591c77
Diego Orefici <orefici.diego@gmail.com>
2021-02-24 15:12:51 -0300

    Middlewares located in the right spot.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 5000;
 
 require("./db");
 
-app.use("/api/posts", require("./router/api/posts"));
-app.use("/api/categories", require("./router/api/categories"));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/posts", require("./router/api/posts"));
+app.use("/api/categories", require("./router/api/categories"));
 
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`);
